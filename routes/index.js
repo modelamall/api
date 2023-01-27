88e8c73dd9c8ef9b686aa483e1c6b61ab1e10e6a
Abdulrahman Albarghouth <58550474+Abdulrahman-Albarghouth@users.noreply.github.com>
2023-01-27 17:09:14 +0300

    porduct

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,7 @@ router.use("/user", require("../lib/user/routers"));
 router.use("/store", require("../lib/store/routers"));
 router.use("/category", require("../lib/category/routers"));
 router.use("/productCode", require("../lib/productCode/routers"));
+router.use("/product", require("../lib/product/routers"));
+router.use("/province", require("../lib/province/routers"));
 
 module.exports = router;
