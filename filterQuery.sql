6e7ff081c9c7f92b867d2a637b68f3ebdbf15c94
Feras Jobeir <fjobeir@gmail.com>
2023-02-22 15:59:36 +0300

    ..

--- filterQuery.sql
+++ filterQuery.sql
@@ -2,28 +2,44 @@ SELECT
     *
 FROM
     `products`
+-- JOIN
 WHERE
-    `categoryId` IN (SELECT id FROM Categories WHERE ID IN(12)) AND EXISTS(
-    SELECT
-        *
-    FROM
-        `productVariations`
-    WHERE
-        `products`.`id` = `productVariations`.`productId` AND(
-            `price` > 4000 AND EXISTS(
-            SELECT
-                *
-            FROM
-                `productProperties`
-            WHERE
-                `productVariations`.`id` = `productProperties`.`productVariationId` AND `propertyId` = 1 AND `propertyValueId` IN(18)
-        ) OR EXISTS(
+    `categoryId` IN (
+        SELECT
+            id
+        FROM
+            Categories
+        WHERE
+            ID IN(12)
+    )
+    AND EXISTS(
         SELECT
             *
         FROM
-            `productProperties`
+            `productVariations`
         WHERE
-            `productVariations`.`id` = `productProperties`.`productVariationId` AND `propertyId` = 8 AND `propertyValueId` IN(22)
-    )
-        )
-);
+            `products`.`id` = `productVariations`.`productId`
+            AND(
+                `price` > 4000
+                AND EXISTS(
+                    SELECT
+                        *
+                    FROM
+                        `productProperties`
+                    WHERE
+                        `productVariations`.`id` = `productProperties`.`productVariationId`
+                        AND `propertyId` = 1
+                        AND `propertyValueId` IN(18)
+                )
+                OR EXISTS(
+                    SELECT
+                        *
+                    FROM
+                        `productProperties`
+                    WHERE
+                        `productVariations`.`id` = `productProperties`.`productVariationId`
+                        AND `propertyId` = 8
+                        AND `propertyValueId` IN(22)
+                )
+            )
+    );
